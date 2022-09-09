ba33045fae3561207bce0c398c468f7536452aab
Igor Lucci <igorlucci@hotmail.com>
2022-09-08 23:26:34 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,29 +3,25 @@ import dash_core_components as dcc
 import dash_html_components as html
 
 app = dash.Dash()
-
-server = app.server
+application = app.server
 
 app.layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
+html.H1(children=’Hello Dash’),
 
-    html.Div(children='''
-        This is Dash running on Azure App Service.
-    '''),
+html.Div(children=’’’
+This is Dash running on Azure App Service.‘’’),
 
-    dcc.Graph(
-        id='example-graph',
-        figure={
-            'data': [
-                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
-                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
-            ],
-            'layout': {
-                'title': 'Dash Data Visualization'
-            }
-        }
-    )
-])
-
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port='80')
+dcc.Graph(
+id=’example-graph’,
+figure={
+‘data’: [
+{‘x’: [1, 2, 3], ‘y’: [4, 1, 2], ‘type’: ‘bar’, ‘name’: ‘SF’},
+{‘x’: [1, 2, 3], ‘y’: [2, 4, 5], ‘type’: ‘bar’, ‘name’: ‘USA’},
+],
+‘layout’: {
+‘title’: ‘Dash Data Visualization’
+}
+}
+)
+if __name__ == ‘__main__’:
+application.run(debug=True, host=’0.0.0.0', port=’3000')
